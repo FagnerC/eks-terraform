ddcf3f383584477014bca6635275122f4a337efd
Fagner Costa <fagner.costa18@hotmail.com>
2024-04-21 11:40:35 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.38.0"
     }
   }
+  backend "s3" {
+    bucket = "s3-terraform-state-eks"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+
+
 }
 
 provider "aws" {
