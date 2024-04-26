084ae69d06b4be4924bda96c1a928388053795ba
Fagner Costa <fagner.costa18@hotmail.com>
2024-04-25 23:27:00 -0300

    feat: output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,15 @@
+output "subnet-pub-1a" {
+  value = aws_subnet.eks-aws-subnet-public-1a
+}
+
+output "subnet-pub-1b" {
+  value = aws_subnet.eks-aws-subnet-public-1a
+}
+
+output "subnet-priv-1a" {
+  value = aws_subnet.eks-aws-subnet-private-1a
+}
+
+output "subnet-priv-1b" {
+  value = aws_subnet.eks-aws-subnet-private-1b
+}   
