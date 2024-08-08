d6a36c19ce66a19f344a5cf4a887958d80c5adb8
Fagner Costa <fagner.costa18@hotmail.com>
2024-08-07 21:14:43 -0300

    fix: insert id to obtain subnet id

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -1,15 +1,15 @@
 output "subnet-pub-1a" {
-  value = aws_subnet.eks-aws-subnet-public-1a
+  value = aws_subnet.eks-aws-subnet-public-1a.id
 }
 
 output "subnet-pub-1b" {
-  value = aws_subnet.eks-aws-subnet-public-1b
+  value = aws_subnet.eks-aws-subnet-public-1b.id
 }
 
 output "subnet-priv-1a" {
-  value = aws_subnet.eks-aws-subnet-private-1a
+  value = aws_subnet.eks-aws-subnet-private-1a.id
 }
 
 output "subnet-priv-1b" {
-  value = aws_subnet.eks-aws-subnet-private-1b
+  value = aws_subnet.eks-aws-subnet-private-1b.id
 }   
