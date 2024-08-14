535930fd0d7be8005997462c168e070c4ce81852
Fagner Costa <fagner.costa18@hotmail.com>
2024-08-14 13:09:55 -0300

    feat: new cluster module

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "subnet_pub_1a" {
-  value = module.eks_network.subnet-pub-1a
+output "eks_vpc_config" {
+  value = module.eks_cluster.eks_vpc_config
+}
+
+output "oidc" {
+  value = module.eks_cluster.oidc
 }
