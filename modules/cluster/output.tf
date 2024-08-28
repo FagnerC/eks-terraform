b2d4b979a8d593eaaa87664866b342045d0049a2
Fagner Costa <fagner.costa18@hotmail.com>
2024-08-28 19:29:21 -0300

    feat: added Controller Module

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -3,9 +3,17 @@ output "eks_vpc_config" {
 }
 
 output "oidc" {
-  value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 output "cluster_name" {
   value = aws_eks_cluster.eks_cluster.id
+}
+
+output "certificate_authority" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
--- provider.tf
+++ provider.tf
@@ -1,20 +1,52 @@
+#####################################################################
+#Providers Terraform
+#####################################################################
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.38.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
   }
+
   backend "s3" {
     bucket = "s3-terraform-state-eks"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-
   }
-
-
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
 }
